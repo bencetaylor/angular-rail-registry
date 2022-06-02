82f3daa1663fc10c69ebe5d0cd855f633496fa4e
bencetaylor <bencetaylor@gmail.com>
2022-06-02 20:02:51 +0200

    3 files modified

--- src/app/service/site.service.ts
+++ src/app/service/site.service.ts
@@ -1,8 +1,48 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Site } from '../sites/site/site';
+import { RequestService } from './request.service';
+
+const AUTHOR_URL = 'api/sites';
 
 @Injectable()
 export class SiteService {
+  constructor(private requestService: RequestService) {}
 
-  constructor() { }
+  getSites(): Observable<Site[]> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+    return this.requestService.get<Site[]>(
+      `${AUTHOR_URL}/?deleted=false`,
+      httpOptions
+    );
+  }
 
+  getSite(siteId: number): Observable<any> {
+    return this.requestService.get(`${AUTHOR_URL}/${siteId}`);
+  }
+
+  createSite(site: Site): Observable<any> {
+    return this.requestService.post(`${AUTHOR_URL}/`, site);
+  }
+
+  updateSite(site: Site): Observable<any> {
+    return this.requestService.put(`${AUTHOR_URL}/`, site);
+  }
+
+  // deleteAuthor(author: Site): Observable<any> {
+  //   return this.booksService.getBooks().pipe(
+  //     exhaustMap((res) => {
+  //       if (res.filter((b) => b.authorId === author.id).length > 0) {
+  //         throw new Error('Cannot delete author!');
+  //       }
+  //       author = Object.assign({}, author, { deleted: true });
+  //       return this.updateAuthor(author);
+  //     })
+  //   );
+  // }
 }
--- src/app/sites/site-list/site-list.component.ts
+++ src/app/sites/site-list/site-list.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { SiteService } from '../../service/site.service';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Site } from '../site/site';
 
 @Component({
   selector: 'app-site-list',
   templateUrl: './site-list.component.html',
-  styleUrls: ['./site-list.component.css']
+  styleUrls: ['./site-list.component.css'],
 })
 export class SiteListComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'birthYear',
+    'nationality',
+    'actions',
+  ];
 
-  constructor() { }
+  // $sites: Observable<Site[]> = this.store.pipe(select(selectSites));
+
+  $sites: Observable<Site[]>;
+
+  constructor(private siteService: SiteService, private store: Store) {}
 
   ngOnInit() {
+    this.$sites.subscribe(this.siteService.getSites);
+    this.$sites.forEach((list) => {
+      list.forEach((s) => console.log(s));
+    });
   }
 
+  // onDelete(site: Site): void {
+  //   this.store.dispatch(siteDeleteAction({site}));
+  // }
 }
