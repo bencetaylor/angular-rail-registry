6ec448112767f91fe9ce6ee45e9ca326f35333bf
bencetaylor <bencetaylor@gmail.com>
2022-06-10 11:53:53 +0200

    1 file modified, 1 added

--- src/app/sites/create-site/create-site.component.ts
+++ src/app/sites/create-site/create-site.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-site',
   templateUrl: './create-site.component.html',
-  styleUrls: ['./create-site.component.css']
+  styleUrls: ['./create-site.component.css'],
 })
 export class CreateSiteComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
